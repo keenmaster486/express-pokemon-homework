841e50322147763c55f02f17c4ea4385fb122f3f
keenmaster486 <cebrock106@gmail.com>
2019-04-12 21:34:12 -0600

    My server is set up and running

---
+++ pokemon.js
@@ -0,0 +1,9 @@
+//Here be the pokemon array
+
+pokemon = [
+{name: "testname1"},
+{name: "testname2"},
+{name: "testname3"}
+];
+
+module.exports = pokemon;
---
+++ server.js
@@ -0,0 +1,33 @@
+console.log("server.js loaded");
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const pokemon = require("./pokemon");
+
+
+app.get('/', function(req, res)
+{
+	res.send("Go to /pokemon");
+});
+
+
+app.get('/pokemon', function(req, res)
+{
+	
+})
+
+
+
+
+app.listen(port, function()
+{
+	console.log(`listening on port ${port}`)
+});
