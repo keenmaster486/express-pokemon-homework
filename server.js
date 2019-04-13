adda6e188cdd72d879c78e3933fef3ab8e5688b4
keenmaster486 <cebrock106@gmail.com>
2019-04-13 17:07:31 -0600

    Bugfixes, works with npm test now, and edit function

--- server.js
+++ server.js
@@ -23,15 +23,39 @@ app.get('/', function(req, res)
 app.get('/pokemon', function(req, res)
 {
 	res.render('index.ejs', {pokemon: pokemon});
-})
-
-app.get('/pokemon/:id', function(req, res)
-{
-	res.render('show.ejs', {pokemon: pokemon[req.params.id]});
 });
 
+app.get('/pokemon/new', function(req, res)
+{
+	res.render('new.ejs', {});
+});
+
+app.post('/pokemon', function(req, res)
+{
+	pokemon.push(req.body);
+	res.redirect('/pokemon');
+});
 
 app.listen(port, function()
 {
 	console.log(`listening on port ${port}`)
 });
+
+app.get('/pokemon/:id', function(req, res)
+{
+	res.render('show.ejs', {pokemon: pokemon[req.params.id], id:req.params.id});
+});
+
+app.get('/pokemon/:id/edit', function(req, res)
+{
+	res.render('edit.ejs', {pokemon: pokemon[req.params.id], id:req.params.id});
+});
+
+app.post('/pokemon/:id', function(req, res)
+{
+	pokemon[req.params.id] = req.body;
+	res.redirect('/pokemon');
+});
+
+
+module.exports = app;
